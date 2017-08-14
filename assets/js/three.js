0596c04ec7e1ceb1557affcae159dadc4fe41e2e
Andrew Eskenazi <drewesk@gmail.com>
2017-08-13 19:10:29 -0600

    Texture Mapping on Moving Objects🏓

--- assets/js/three.js
+++ assets/js/three.js
@@ -7,20 +7,69 @@ const REND = new THREE.WebGLRenderer({canvas: document.getElementById('targetCan
                                         antialias: true
                                       });
 
-REND.setClearColor(0xDAF7A6);
+REND.setClearColor(0x3F3E3E);
 REND.setPixelRatio(window.devicePixelRatio);
 REND.setSize(window.innerWidth, window.innerHeight);
 
 const CAM = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 0.1, 3000);
-const SC = new THREE.Scene();
+const scene = new THREE.Scene();
 
 const LIGHT = new THREE.AmbientLight(0xffffff, 0.7);
-SC.add(LIGHT);
+scene.add(LIGHT);
 
 const LIGHT_REF = new THREE.PointLight(0xffffff, 0.6);
-SC.add(LIGHT_REF);
+scene.add(LIGHT_REF);
 
-const GEO = new THREE.PlaneGeometry(100, 100);
+var loader = new THREE.TextureLoader();
+loader.setCrossOrigin("anonymous");
+var groundTexture = loader.load( 'https://cdn.shopify.com/s/files/1/1297/3303/products/WE066220-2_grande.jpg?v=1490394992' );
+groundTexture.wrapS = groundTexture.wrapT = THREE.RepeatWrapping;
+groundTexture.repeat.set( 25, 25 );
+groundTexture.anisotropy = 16;
+
+const material = new THREE.MeshPhongMaterial({
+  color: 0x87E5EA,
+  map: groundTexture
+  // transparent: true,
+  // opacity: 0.95,
+  // wireframe: true,
+});
+
+const planeMaterial = new THREE.MeshLambertMaterial({
+  color: 0x17202A
+})
+
+
+const geometry = new THREE.BoxGeometry(100, 100, 100);
+geometry.computeLineDistances();
+const mesh = new THREE.Mesh(geometry, material);
+mesh.position.z = -500;
+mesh.position.x = -100;
+scene.add(mesh);
+
+const geometry2 = new THREE.SphereGeometry(50, 20, 20);
+const mesh2 = new THREE.Mesh(geometry2, material);
+mesh2.position.z = -500;
+mesh2.position.x = 100;
+scene.add(mesh2);
+
+const geometry3 = new THREE.PlaneGeometry(10000, 10000, 100, 100);
+const mesh3 = new THREE.Mesh(geometry3, planeMaterial);
+mesh3.rotation.x = -90 * Math.PI / 180;
+mesh3.position.y = -100;
+scene.add(mesh3);
+
+const geometry4 = new THREE.SphereGeometry(20, 10, 10);
+const mesh4 = new THREE.Mesh(geometry4, material);
+mesh4.position.z = -500;
+mesh4.position.x = -50;
+scene.add(mesh4);
+
+
+// =================================================
+// =================================================
+
+// const GEO = new THREE.PlaneGeometry(100, 100);
 
 // =================================================
 // =================================================
@@ -95,26 +144,27 @@ const GEO = new THREE.PlaneGeometry(100, 100);
 // =================================================
 // =================================================
 
-const MAT = new THREE.MeshLambertMaterial({color: 0xffa07a});
-const MESH = new THREE.Mesh(GEO, MAT);
-
-MESH.position.z = -1000;
+// MESH.position.z = -1000;
 // MESH.position.set(0, 0, -1000);
-
-SC.add(MESH);
 
 // requestAnimationFrame(render);
 render();
 
-var delta = 0; // ask CJ why let is not an equivalent
+// const delta = 0; // ask CJ why let is not an equivalent
 
 function render() {
-  // MESH.rotation.x += 0.01;
-  // MESH.rotation.y += 0.01;
-  delta += 0.06;
-  GEO.vertices[0].x = -25 + Math.sin(delta) * 50;
-  GEO.verticesNeedUpdate = true;
+  mesh.rotation.x += 0.007;
+  mesh.rotation.y += 0.007;
 
-  REND.render(SC, CAM);
+  mesh4.rotation.x += 0.007;
+  mesh4.rotation.y += 0.007;
+
+  mesh2.rotation.x += 0.007;
+  mesh2.rotation.y += 0.007;
+  // delta += 0.06;
+  // GEO.vertices[0].x = -25 + Math.sin(delta) * 50;
+  // GEO.verticesNeedUpdate = true;
+
+  REND.render(scene, CAM);
   requestAnimationFrame(render);
 }
