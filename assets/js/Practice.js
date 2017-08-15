54d1c3ae5d632698876d7d03b210b3e8f755dffb
Andrew Eskenazi <drewesk@gmail.com>
2017-08-15 16:04:56 -0600

    Updating the Geometry Metrics

--- assets/js/Practice.js
+++ assets/js/Practice.js
@@ -9,10 +9,10 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const LIGHT = new THREE.AmbientLight(0xffffff, 0.7);
+const LIGHT = new THREE.AmbientLight(0xffffff, 0.5);
 scene.add(LIGHT);
 
-const LIGHT_REF = new THREE.PointLight(0xffffff, 0.6);
+const LIGHT_REF = new THREE.PointLight(0xffffff, 0.95);
 scene.add(LIGHT_REF);
 
 const material = new THREE.MeshLambertMaterial({
@@ -24,15 +24,51 @@ const material2 = new THREE.MeshPhongMaterial({
   color: 0xEB650f,
 });
 
+const material3 = new THREE.MeshLambertMaterial({
+  color: 0x0febcb,
+  wireframe: true
+});
+
+const material4 = new THREE.MeshLambertMaterial({
+  color: 0xc3eb0f,
+  transparent: true,
+  opacity: 0.4
+});
+
+const material5 = new THREE.MeshLambertMaterial({
+  color: 0x0febcb,
+  wireframe: true
+  // transparent: true,
+  // opacity: 0.2
+});
+
+const material6 = new THREE.MeshLambertMaterial({
+  color: 0x0DFCE6,
+  wireframe: true,
+  transparent: true,
+  opacity: 0.5
+});
+
 // go over the docs!
 
-const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+const geometry = new THREE.TorusGeometry(37, 10, 16, 100);
 const geometry2 = new THREE.IcosahedronGeometry(10, 0);
 const geometry3 = new THREE.IcosahedronGeometry(50, 2);
+const geometry4 = new THREE.IcosahedronGeometry(20, 0);
+const geometry5 = new THREE.IcosahedronGeometry(10, 0);
 
-const donut = new THREE.Mesh(geometry, material);
+// const donut = new THREE.Mesh(geometry, material5);
 const icos = new THREE.Mesh(geometry2, material2);
-const icos2 = new THREE.Mesh(geometry3, material);
+const icos2 = new THREE.Mesh(geometry3, material6);
+
+const icosAlias = new THREE.Mesh(geometry2, material3);
+const icos2Alias = new THREE.Mesh(geometry4, material4);
+
+const molecule = new THREE.Mesh(geometry4, material);
+const moleculeAlias = new THREE.Mesh(geometry5, material);
+
+// donut.position.z = -200;
+// donut.position.x = 0;
 
 icos.position.z = -200;
 icos.position.x = 0;
@@ -40,21 +76,44 @@ icos.position.x = 0;
 icos2.position.z = -200;
 icos2.position.x = 0;
 
+molecule.position.z = -200;
+molecule.position.x = 0;
+
+moleculeAlias.position.z = -200;
+moleculeAlias.position.x = 150;
+
+icosAlias.position.z = -200;
+icosAlias.position.x = 90;
+
+icos2Alias.position.z = -200;
+icos2Alias.position.x = 90;
+
 scene.add(icos);
 scene.add(icos2);
-scene.add(donut);
+// scene.add(donut);
+scene.add(molecule);
+scene.add(moleculeAlias);
 
-camera.position.z = 50;
+scene.add(icosAlias);
+scene.add(icos2Alias);
+
+camera.position.z = 30;
 
 function animate() {
-  donut.rotation.x += 0.007;
-  donut.rotation.y += 0.007;
+  // donut.rotation.x += 0.003;
+  // donut.rotation.y += 0.003;
 
-  icos.rotation.x += 0.007;
-  icos.rotation.y += 0.007;
+  icos.rotation.x += 0.004;
+  icos.rotation.y += 0.004;
 
-  icos2.rotation.x += 0.007;
-  icos2.rotation.y += 0.007;
+  icos2.rotation.x += 0.001;
+  icos2.rotation.y += 0.001;
+
+  icosAlias.rotation.x += 0.004;
+  icosAlias.rotation.y += 0.004;
+
+  icos2Alias.rotation.x += 0.001;
+  icos2Alias.rotation.y += 0.001;
 
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
