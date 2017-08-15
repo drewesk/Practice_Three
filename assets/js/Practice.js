bd4921f5d2b67f18e789edb659d1f75cbdc93cd8
Andrew Eskenazi <drewesk@gmail.com>
2017-08-15 17:00:56 -0600

    Refactor of the practice directory.

--- assets/js/Practice.js
+++ assets/js/Practice.js
@@ -4,6 +4,8 @@ const renderer = new THREE.WebGLRenderer({
   antialias: true,
 });
 
+let spheres = [];
+
 renderer.setSize( window.innerWidth, window.innerHeight );
 
 const scene = new THREE.Scene();
@@ -44,9 +46,24 @@ const material5 = new THREE.MeshLambertMaterial({
 
 const material6 = new THREE.MeshLambertMaterial({
   color: 0x0DFCE6,
-  wireframe: true,
+  wireframe: true
+});
+
+const material7 = new THREE.MeshLambertMaterial({
+  color: 0x0BFC8D,
+  // wireframe: true,
   transparent: true,
-  opacity: 0.5
+  opacity: 0.8,
+  emissive: 0xff0000,
+  emissiveIntensity: 0.9
+});
+
+const material8 = new THREE.MeshLambertMaterial({
+  color: 0x0DFCE6,
+  transparent: true,
+  opacity: 0.3,
+  emissive: 0xff0000,
+  emissiveIntensity: 0.9
 });
 
 // go over the docs!
@@ -56,19 +73,33 @@ const geometry2 = new THREE.IcosahedronGeometry(10, 0);
 const geometry3 = new THREE.IcosahedronGeometry(50, 2);
 const geometry4 = new THREE.IcosahedronGeometry(20, 0);
 const geometry5 = new THREE.IcosahedronGeometry(10, 0);
+const geometry6 = new THREE.IcosahedronGeometry(55, 2);
+const geometry7 = new THREE.IcosahedronGeometry(12, 0);
+const geometry8 = new THREE.IcosahedronGeometry(6, 0);
 
 // const donut = new THREE.Mesh(geometry, material5);
 const icos = new THREE.Mesh(geometry2, material2);
 const icos2 = new THREE.Mesh(geometry3, material6);
+const icos3 = new THREE.Mesh(geometry6, material7);
 
 const icosAlias = new THREE.Mesh(geometry2, material3);
 const icos2Alias = new THREE.Mesh(geometry4, material4);
 
 const molecule = new THREE.Mesh(geometry4, material);
-const moleculeAlias = new THREE.Mesh(geometry5, material);
+const moleculeAlias = new THREE.Mesh(geometry8, material);
+const moleculeGlow = new THREE.Mesh(geometry7, material8);
 
-// donut.position.z = -200;
-// donut.position.x = 0;
+const sphere = new THREE.SphereBufferGeometry( 0.1, 32, 16 );
+				const sphereGlow = new THREE.MeshBasicMaterial( { color: 0xffffff } );
+				for ( let i = 0; i < 100; i ++ ) {
+					let mesh = new THREE.Mesh( sphere, sphereGlow );
+					mesh.position.x = Math.random() * 20 - 2;
+					mesh.position.y = Math.random() * 11 - 9;
+					mesh.position.z = Math.random() * 11 - 2;
+					mesh.scale.x = mesh.scale.y = mesh.scale.z = Math.random() * 3 + 1;
+					scene.add( mesh );
+					spheres.push( mesh );
+				}
 
 icos.position.z = -200;
 icos.position.x = 0;
@@ -76,11 +107,17 @@ icos.position.x = 0;
 icos2.position.z = -200;
 icos2.position.x = 0;
 
+icos3.position.z = -200;
+icos3.position.x = 0;
+
 molecule.position.z = -200;
 molecule.position.x = 0;
 
 moleculeAlias.position.z = -200;
 moleculeAlias.position.x = 150;
+
+moleculeGlow.position.z = -200;
+moleculeGlow.position.x = 150;
 
 icosAlias.position.z = -200;
 icosAlias.position.x = 90;
@@ -90,9 +127,13 @@ icos2Alias.position.x = 90;
 
 scene.add(icos);
 scene.add(icos2);
+scene.add(icos3);
+
 // scene.add(donut);
+
 scene.add(molecule);
 scene.add(moleculeAlias);
+scene.add(moleculeGlow);
 
 scene.add(icosAlias);
 scene.add(icos2Alias);
