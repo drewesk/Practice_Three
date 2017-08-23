f92c3012bf28df9343932e1854daa2a4c7e58502
Andrew Eskenazi <drewesk@gmail.com>
2017-08-22 23:19:49 -0600

    Experimenting more with three.js functionality

--- assets/js/Practice.js
+++ assets/js/Practice.js
@@ -24,6 +24,8 @@ const material = new THREE.MeshLambertMaterial({
 
 const material2 = new THREE.MeshPhongMaterial({
   color: 0xEB650f,
+  transparent: true,
+  opacity: 0.9
 });
 
 const material3 = new THREE.MeshLambertMaterial({
@@ -67,13 +69,12 @@ const material8 = new THREE.MeshLambertMaterial({
 });
 
 // go over the docs!
-
 const geometry = new THREE.TorusGeometry(37, 10, 16, 100);
-const geometry2 = new THREE.IcosahedronGeometry(10, 0);
+const geometry2 = new THREE.IcosahedronGeometry(10, 5);
 const geometry3 = new THREE.IcosahedronGeometry(50, 2);
 const geometry4 = new THREE.IcosahedronGeometry(20, 0);
 const geometry5 = new THREE.IcosahedronGeometry(10, 0);
-const geometry6 = new THREE.IcosahedronGeometry(55, 2);
+const geometry6 = new THREE.IcosahedronGeometry(55, 4);
 const geometry7 = new THREE.IcosahedronGeometry(12, 0);
 const geometry8 = new THREE.IcosahedronGeometry(6, 0);
 
@@ -91,15 +92,22 @@ const moleculeGlow = new THREE.Mesh(geometry7, material8);
 
 icos.position.z = -200;
 icos.position.x = 0;
+icos.position.z = -15;
 
 icos2.position.z = -200;
 icos2.position.x = 0;
+icos2.position.z = -15;
+
 
 icos3.position.z = -200;
 icos3.position.x = 0;
+icos3.position.z = -15;
+
 
 molecule.position.z = -200;
 molecule.position.x = 0;
+molecule.position.z = -15;
+
 
 moleculeAlias.position.z = -200;
 moleculeAlias.position.x = 150;
@@ -115,40 +123,59 @@ icos2Alias.position.x = 90;
 
 scene.add(icos);
 scene.add(icos2);
-scene.add(icos3);
+// scene.add(icos3);
 
 // scene.add(donut);
 
-scene.add(molecule);
-scene.add(moleculeAlias);
-scene.add(moleculeGlow);
+// scene.add(molecule);
+// scene.add(moleculeAlias);
+// scene.add(moleculeGlow);
 
-scene.add(icosAlias);
-scene.add(icos2Alias);
+// scene.add(icosAlias);
+// scene.add(icos2Alias);
 const sphere = new THREE.SphereBufferGeometry(0.06, 32, 16);
 
 
-for (let i = 0; i < 600; i++) {
+for (let i = 0; i < 400; i++) {
 
   let randomColor = Math.random() * 0xffffff;//`#${Math.floor(Math.random()*16777215).toString(16)}`;
 
-  let sphereGlow = new THREE.MeshBasicMaterial({color: randomColor});
+  let sphereGlow = new THREE.MeshLambertMaterial({
+    color: randomColor,
+    transparent: true,
+    opacity: 0.5,
+    emissive: 0xff0000,
+    emissiveIntensity: 0.5
+  });
   let mesh = new THREE.Mesh(sphere, sphereGlow);
 
-  mesh.position.x = Math.random() * 20 - 18;
-  mesh.position.y = Math.random() * 11 - 9;
-  mesh.position.z = Math.random() * 11 - 2;
-  mesh.scale.x = mesh.scale.y = Math.random() * 3 + 1;
-  mesh.scale.z = 20;
-  scene.add(mesh);
-  spheres.push(mesh);
+
+  	// mesh.position.x = Math.random() * 10000 - 5000;
+  	// mesh.position.y = Math.random() * 10000 - 5000;
+  	// mesh.position.z = Math.random() * 10000 - 5000;
+
+  	mesh.scale.x = mesh.scale.y = mesh.scale.z = Math.random() * 10;
+  	scene.add( mesh );
+    mesh.position.y = Math.random() * 1;
+    mesh.position.z = Math.random() * 1;
+    // mesh.scale.x = mesh.scale.y = Math.random() * 3 + 1;
+    mesh.scale.z = 10;
+    // scene.add(mesh);
+    spheres.push(mesh);
 }
 
 camera.position.z = 30;
 
 function animate() {
-  // donut.rotation.x += 0.003;
-  // donut.rotation.y += 0.003;
+
+  let timer = 0.0001 * Date.now();
+
+  for(let i = 0, spLength = spheres.length; i < spLength; i++) {
+    let sphere = spheres[i];
+
+    sphere.position.x = 10 * Math.cos( timer + i );
+    sphere.position.y = 10 * Math.sin( timer + i * 1.1 );
+  }
 
   icos.rotation.x += 0.004;
   icos.rotation.y += 0.004;
@@ -163,6 +190,7 @@ function animate() {
   icos2Alias.rotation.y += 0.001;
 
   requestAnimationFrame(animate);
+
   renderer.render(scene, camera);
 }
 
